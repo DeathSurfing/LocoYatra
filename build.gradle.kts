2add046019ebf6737f3a1a37a55ed51873c6a7a6
Aditya Vikram Mahendru <70932037+DeathSurfing@users.noreply.github.com>
2024-09-09 15:45:27 +0530

    Added unity folder for management
    Unity managemenet added

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,24 @@
  * This is a general purpose Gradle build.
  * Learn more about Gradle by exploring our Samples at https://docs.gradle.org/8.10/samples
  */
+ plugins {
+    id 'com.android.application'
+    id 'kotlin-android'
+}
+
+android {
+    compileSdkVersion 31
+    defaultConfig {
+        applicationId "com.example.arcrossplatformapp"
+        minSdkVersion 24
+        targetSdkVersion 31
+        versionCode 1
+        versionName "1.0"
+    }
+}
+
+dependencies {
+    implementation "com.google.ar:core:1.34.0"  // Latest ARCore version
+    implementation "org.jetbrains.kotlin:kotlin-stdlib:1.8.0"  // Latest Kotlin version
+}
+
